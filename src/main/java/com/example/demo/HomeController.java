0673986801d0410e1d39a0e9c027bdc60b41b32b
shan56 <shan56@montgomerycollege.edu>
2020-07-16 15:32:24 -0400

    includes fragments, update, & delete

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,10 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -14,20 +11,62 @@ import java.util.ArrayList;
 public class HomeController {
     public ArrayList<Employee> allEmployees = new ArrayList<>();
 
+    @RequestMapping("/home")
+    public String index(){
+        return "index";
+    }
+
     @GetMapping("/add")
-    public String index(Model model) {
+    public String add(Model model) {
         model.addAttribute("employee", new Employee());
-        model.addAttribute("employees", allEmployees);
+        model.addAttribute("message", "New Employee");
         return "addEmployee";
     }
 
     @PostMapping("/listAll")
     public String listAll(@ModelAttribute Employee employee, Model model){
+
         allEmployees.add(employee);
+        return "redirect:/showAll";
+    }
+
+
+    @RequestMapping("/showAll")
+    public String showAll(Model model){
+
         model.addAttribute("employees", allEmployees);
-        model.addAttribute("employee", new Employee());
-        //return "listAll";
+        return "listAll";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateList(@PathVariable("id") long id, Model model){
+
+        Employee employee = new Employee();
+        for (Employee one : allEmployees) {
+            if (one.getId() == id){
+                employee = one;
+                allEmployees.remove(one);
+                break;
+            }
+        }
+
+        model.addAttribute("employee", employee);
+        model.addAttribute("message", "Update Employee");
         return "addEmployee";
     }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteList(@PathVariable("id") long id, Model model){
+
+        for (Employee employee : allEmployees) {
+            if (employee.getId() == id){
+                allEmployees.remove(employee);
+                break;
+            }
+        }
+
+        return "redirect:/showAll";
+    }
+
 
 }
--- src/test/java/com/example/demo/PassingIDToControllerTests.java
+++ src/test/java/com/example/demo/PassingIDToControllerTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ForEachExampleApplicationTests {
+class PassingIDToControllerTests {
 
     @Test
     void contextLoads() {
